f06a7ac524b2e34ab30a32919932ee3bc0d68c35
Alan Pennacchio <alanpennacchio@icloud.com>
2020-04-25 13:47:39 -0300

    Add step_dummy

--- recette/steps.py
+++ recette/steps.py
@@ -1,4 +1,5 @@
 from typing import Callable, List
+from uuid import uuid4
 
 import pandas as pd
 
@@ -55,3 +56,72 @@ def step_other_learner(
         return df
 
     return step_other
+
+
+def step_dummy_learner(
+    df: pd.DataFrame,
+    cols: List[str],
+    drop_first: bool = True,
+    keep_cols: bool = False,
+    sep: str = "=",
+) -> StepType:
+    """Convert categorical columns to dummy columns.
+
+    Args:
+        df: Pandas DataFrame to process.
+        cols: Columns to process.
+        drop_first: For k categories, create k - 1 dummy variables instead of k
+            by removing the first category.
+        keep_cols: Keep processed columns (in addition to dummy columns).
+        sep: Separator between column name and category for dummy column names.
+
+    Returns:
+        Step that converts categorical columns to dummy columns.
+
+    """
+    # Store known categories for each column, dropping the first if appropriate
+    cols_cats = [df.loc[:, col].cat.categories[int(drop_first) :] for col in cols]
+
+    def step_dummy(df: pd.DataFrame) -> pd.DataFrame:
+        original_df, df = df, df.copy()
+
+        # Convert all unknown categories to temporary ones
+        tmp_cats = []
+        for col, cats in zip(cols, cols_cats):
+            # Get unknown categories
+            unknown_cats = df.loc[:, col].cat.categories.difference(cats)
+
+            # Make temporary category
+            tmp_cat = str(uuid4())  # Ensure no collapse with any category
+            tmp_cats.append(tmp_cat)
+            df.loc[:, col] = df.loc[:, col].cat.add_categories(tmp_cat)
+
+            # Convert unknown categories to it
+            df.loc[df.loc[:, col].isin(unknown_cats), col] = tmp_cat
+
+            # Remove unknown categories and set the remaining categories order
+            df.loc[:, col] = (
+                df.loc[:, col]
+                .cat.remove_categories(unknown_cats)
+                .cat.set_categories(cats.insert(0, tmp_cat))
+            )
+
+        dummies_df = pd.get_dummies(df.loc[:, cols], columns=cols, prefix_sep=sep)
+
+        # Remove temporary category dummies
+        tmp_dummy_cols = dummies_df.columns.intersection(
+            [col + sep + str(tmp_cat) for col, tmp_cat in zip(cols, tmp_cats)]
+        )
+        dummies_df = dummies_df.drop(columns=tmp_dummy_cols)
+
+        # Propagate NAs
+        for col, cats in zip(cols, cols_cats):
+            dummy_cols = [col + sep + str(cat) for cat in cats]
+            dummies_df.loc[df.loc[:, col].isna(), dummy_cols] = pd.NA
+
+        # Cast dummies to nullable integer type
+        dummies_df = dummies_df.astype("Int64")
+
+        return original_df.drop(columns=[] if keep_cols else cols).join(dummies_df)
+
+    return step_dummy
--- tests/test_steps.py
+++ tests/test_steps.py
@@ -1,11 +1,10 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-from recette.steps import step_other_learner
+from recette.steps import step_dummy_learner, step_other_learner
 
 
 def test_step_other_learner():
-    # Input data. Note that categorical columns are used
     dtypes = {"color": "category", "animal": "category", "size": "Int64"}
     train_df = pd.DataFrame(
         {
@@ -22,13 +21,11 @@ def test_step_other_learner():
         }
     ).astype(dtypes)
 
-    # Compute output
     step_other = step_other_learner(
         train_df, cols=["color", "animal"], threshold=0.5, other_cat="abcd"
     )
     output_df = step_other(test_df)
 
-    # Check if it's right
     expected_df = pd.DataFrame(
         {
             "color": pd.Categorical(
@@ -39,4 +36,41 @@ def test_step_other_learner():
             "size": [11, None, 13, 17, 19, 23],
         }
     ).astype(dtypes)
+    assert_frame_equal(output_df, expected_df)
+
+
+def test_step_dummy_learner():
+    dtypes = {"color": "category", "animal": "category", "size": "Int64"}
+    train_df = pd.DataFrame(
+        {
+            "color": pd.Categorical(
+                ["red", "red", None, "blue", "blue", "blue"], categories=["red", "blue"]
+            ),
+            "animal": pd.Categorical(
+                ["cat", "cat", "cat", "dog", "dog", "rat"],
+                categories=["rat", "cat", "dog"],
+            ),
+            "size": [None, 1, 2, 3, 5, 7],
+        }
+    ).astype(dtypes)
+    test_df = pd.DataFrame(
+        {
+            "color": ["red", "red", "yellow", "blue", "blue", "green"],
+            "animal": ["cat", None, "cat", "cat", "cat", "dog"],
+            "size": [11, None, 13, 17, 19, 23],
+        }
+    ).astype(dtypes)
+
+    step_dummy = step_dummy_learner(train_df, cols=["color", "animal"], sep="_")
+    output_df = step_dummy(test_df)
+
+    expected_df = pd.DataFrame(
+        {
+            "size": [11, None, 13, 17, 19, 23],
+            "color_blue": [0, 0, 0, 1, 1, 0],
+            "animal_cat": [1, None, 1, 1, 1, 0],
+            "animal_dog": [0, None, 0, 0, 0, 1],
+        },
+        dtype="Int64",
+    )
     assert_frame_equal(output_df, expected_df)
