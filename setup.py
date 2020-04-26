68590e69202dd3c5473cba83c4dc6de0c2306f83
Alan Pennacchio <alanpennacchio@icloud.com>
2020-04-26 14:33:04 -0300

    Make package installable
    Removed requirements.txt, since the dependencies are already declared in
    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="recette",
+    version="0.1.0",
+    author="Alan Pennacchio",
+    author_email="alanpennacchio@icloud.com",
+    description="Data preprocessing utilities on top of pandas.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pennacchio/recette",
+    packages=["recette"],
+    python_requires=">=3.6",
+    install_requires=["pandas >= 1.0.3"],
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
