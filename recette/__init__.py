1749b0d7763498420102588e377d1c68fd0df19f
Alan Pennacchio <alanpennacchio@icloud.com>
2020-06-28 20:16:29 -0300

    Bump version to 0.2.1

--- recette/__init__.py
+++ recette/__init__.py
@@ -2,4 +2,4 @@ from recette.steps import prep_step_dummy, prep_step_other
 from recette.utils import combine
 
 # Package version single source of truth
-__version__ = "0.2.0"
+__version__ = "0.2.1"
